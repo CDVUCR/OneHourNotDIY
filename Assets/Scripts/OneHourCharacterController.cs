a2ae17e4a6e1f94f0f8bc3d2ce5a1460e68d8113
Jose Monge <jmonge@headlesschickengames.com>
2017-06-13 19:55:42 -0600

    One Hour game

---
+++ Assets/Scripts/OneHourCharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneHourCharacterController : MonoBehaviour {
+
+	private Rigidbody myRigidBody;
+	public float speed;
+	public float jumpSpeed;
+
+	public bool isGrounded;
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody>();
+		isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Apply a force in the direction of the key press!!!
+		if(Input.GetKey(KeyCode.D))
+		myRigidBody.AddForce(Vector3.right * speed,ForceMode.VelocityChange);
+		
+		if(Input.GetKey(KeyCode.A))
+		myRigidBody.AddForce(Vector3.left * speed,ForceMode.VelocityChange);
+
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			isGrounded = false;
+			myRigidBody.AddForce(Vector3.up * jumpSpeed,ForceMode.VelocityChange);
+		}
+	}
+
+}
